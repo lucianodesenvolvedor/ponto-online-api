3c47a71f1735bfe0c0945d570ab3b1cf53c7f14a
admin <luciano.desenvolvedor@gmail.com>
2020-03-17 18:08:43 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/com/ponto/online/PontoOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.ponto.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ponto/online/PontoOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ponto.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
